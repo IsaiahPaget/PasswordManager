b8a634a95a9f6694ae54359b4676aa8750c6dfb7
Isaiah Paget <113209222+IsaiahPaget@users.noreply.github.com>
2024-09-29 15:40:18 -0700

    update deployment strategy

--- api/PasswordManager.Web/Program.cs
+++ api/PasswordManager.Web/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.EnableDetailedErrors();
-    options.UseSqlServer(builder.Configuration.GetConnectionString("passwordmanager.dev"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
 });
 
 builder.Services.AddIdentity<AppUser, IdentityRole>(options => {
